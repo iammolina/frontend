53937fb1e2f186fbab9d9c2f90d8430453b72141
iammolina <jicayun@aol.com>
2016-11-17 08:24:16 -0300

    estructura - material - jwt

--- app/app.js
+++ app/app.js
@@ -1,1 +1,66 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMaterial','ui.router','angular-jwt']);
+
+app.config(function($stateProvider,$urlRouterProvider,$httpProvider,jwtInterceptorProvider,$mdThemingProvider)
+{
+
+
+  $httpProvider.defaults.useXDomain = true;
+
+
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+
+  jwtInterceptorProvider.tokenGetter = function()
+  {
+       return localStorage.getItem("token");
+  }
+  
+  $httpProvider.interceptors.push("jwtInterceptor");
+
+
+   $mdThemingProvider.theme('default')
+
+      .primaryPalette('orange',
+      {
+      'default': 'A100', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': 'A200', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': 'A400', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': '900' // use shade A100 for the <code>md-hue-3</code> class
+      })
+
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+
+      .accentPalette('light-blue', 
+      {
+        'default': '500' // use shade 200 for default, and keep all other shades the same
+      })
+
+      .warnPalette('red' , 
+      {
+        'default': '500' 
+      })
+
+ $urlRouterProvider.otherwise("/");
+
+  var loginState = 
+  {
+    name: 'login',
+    url: '/',
+    templateUrl: 'app/views/login.html',
+    controller:'mainctrl'
+  }
+
+  var aboutState = 
+  {
+    name: 'home',
+    url: '/home',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+
+  $stateProvider.state(loginState);
+  $stateProvider.state(aboutState);
+
+
+});
